537a76a8dd9eb84bc1487cafcf3eb4582dd7bc82
Stephen <swrigdon@gmail.com>
2017-04-06 19:02:20 -0400

    Added muh Boss Levelz

---
+++ core/src/dungeon/BossLevelGenerator.java
@@ -0,0 +1,7 @@
+package dungeon;
+
+/***************************************
+ * Created by Stephen Rigdon on 4/6/2017.*
+ ***************************************/
+public class BossLevelGenerator {
+}
